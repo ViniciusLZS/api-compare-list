0dac6d2b21cae04a0fc5c587edca028ba4ab0784
Vinícius <vinicius.luiz.9256@gmail.com>
2023-05-16 20:27:51 -0300

    feat: users, lists, measures and products

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,26 @@
 const { Router } = require("express");
 const ListController = require("./app/controllers/ListController");
 const UserController = require("./app/controllers/UserController");
+const ProductController = require("./app/controllers/ProductController");
+const MeasurementController = require("./app/controllers/MeasurementController");
 
 const router = Router();
 
 router.get("/user", UserController.index);
 router.get("/user/:id", UserController.show);
 router.post("/user", UserController.store);
+router.put("/user/:id", UserController.update);
 
-router.get("/list/:id", ListController.index);
+router.get("/list/user/:id", ListController.index);
 router.get("/list/:id", ListController.show);
 router.post("/list", ListController.store);
+
+router.get("/measurement", MeasurementController.index);
+router.get("/measurement/:id", MeasurementController.show);
+router.post("/measurement", MeasurementController.store);
+
+router.get("/product/list/:id", ProductController.index);
+router.get("/product/:id", ProductController.show);
+router.post("/product", ProductController.store);
 
 module.exports = router;
