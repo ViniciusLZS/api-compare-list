8c415694c0633a5d0fd1b636c1a51859821682a6
Vinícius <vinicius.luiz.9256@gmail.com>
2023-05-17 17:26:57 -0300

    feat: added functions

--- src/app/controllers/ListController.js
+++ src/app/controllers/ListController.js
@@ -3,14 +3,14 @@ const isValidUUID = require("../utils/isValidUUID");
 
 class ListController {
   async index(request, response) {
-    const {id} = request.params;
+    const { id } = request.params;
     const { orderBy } = request.query;
 
-    if(id && !isValidUUID(id)) {
-      return response.status(400).json({error: 'Invalid user_id'});
+    if (id && !isValidUUID(id)) {
+      return response.status(400).json({ error: 'Invalid user_id' });
     }
 
-    const list = await ListRepository.findAll({id, orderBy});
+    const list = await ListRepository.findAll({ id, orderBy });
 
     if (!list) {
       return response.status(404).json({ error: 'List not found' });
@@ -50,6 +50,12 @@ class ListController {
       return response.status(400).json({ error: 'Invalid user_id' });
     }
 
+    const userIdExist = await ListRepository.findUserId(user_id);
+
+    if(!userIdExist) {
+      return response.status(404).json({error: 'User_id not found'});
+    }
+
     const list = await ListRepository.create({
       name,
       estimated,
@@ -59,9 +65,61 @@ class ListController {
     response.status(201).json(list);
   }
 
-  update() { }
+  async update(request, response) {
+    const { id } = request.params;
+    const { name, estimated, user_id } = request.body;
 
-  delete() { }
+    if (id && !isValidUUID(id)) {
+      return response.status(400).json({ error: 'Invalid list id' })
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    if (!estimated) {
+      return response.status(400).json({ error: 'Estimated is required' });
+    }
+
+    if (user_id && !isValidUUID(user_id)) {
+      return response.status(400).json({ error: 'Invalid user_id' });
+    }
+
+    const userIdExist = await ListRepository.findUserId(user_id);
+
+    if(!userIdExist) {
+      return response.status(404).json({error: 'User_id not found'});
+    }
+
+    const list = await ListRepository.update(
+      id,
+      {
+        name,
+        estimated,
+        user_id
+      }
+    );
+
+    response.json(list);
+  }
+
+  async delete(request, response) {
+    const {id} = request.params;
+
+    if (!isValidUUID(id)) {
+      return response.status(400).json({ error: 'Invalid list id' });
+    }
+
+    const list = await ListRepository.findById(id);
+
+    if (!list) {
+      return response.status(404).json({ error: 'List not found' });
+    }
+
+    await ListRepository.delete(id);
+
+    response.sendStatus(204);
+  }
 }
 
 module.exports = new ListController();
--- src/app/repositories/ListRepository.js
+++ src/app/repositories/ListRepository.js
@@ -6,7 +6,7 @@ class ListRepository {
     const rows = db.query(`
       SELECT *
       FROM lists
-      WHERE user_id = $1
+      WHERE user_id = $1 AND deleted_at IS NULL
       ORDER BY lists.name ${direction}
     `, [id]);
 
@@ -17,10 +17,20 @@ class ListRepository {
     const [row] = await db.query(`
       SELECT *
       FROM lists
-      WHERE id = $1
+      WHERE id = $1 AND deleted_at IS NULL
     `, [id]);
 
     return row;
+  }
+
+  async findUserId(userId) {
+    const [row] = await db.query(`
+    SELECT *
+    FROM users
+    WHERE id = $1 AND deleted_at IS NULL
+  `, [userId]);
+
+  return row;
   }
 
   async create({ name, estimated, user_id }) {
@@ -31,6 +41,32 @@ class ListRepository {
     `, [name, estimated, user_id]);
 
     return row;
+  }
+
+  async update(id, { name, estimated, user_id }) {
+    const [row] = await db.query(`
+      UPDATE lists
+      SET name = $1, estimated = $2, user_id = $3
+      WHERE id = $4 AND deleted_at IS NULL
+    `, [name, estimated, user_id, id]);
+
+    return row;
+  }
+
+  async delete(id) {
+    const deleteList = await db.query(`
+      UPDATE lists
+      SET deleted_at = NOW()
+      WHERE id = $1
+    `, [id]);
+
+    const deleteProduct = await db.query(`
+      UPDATE products
+      SET deleted_at = NOW()
+      WHERE list_id = $1
+    `, [id]);
+
+    return { deleteList, deleteProduct };
   }
 }
 
