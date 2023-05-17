2beb6b4839226e679a70ed54e727a2be5926186e
Vinícius <vinicius.luiz.9256@gmail.com>
2023-05-17 17:25:29 -0300

    feat: routes

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const ListController = require("./app/controllers/ListController");
 const UserController = require("./app/controllers/UserController");
 const ProductController = require("./app/controllers/ProductController");
-const MeasurementController = require("./app/controllers/MeasurementController");
+const MeasureController = require("./app/controllers/MeasureController");
 
 const router = Router();
 
@@ -10,17 +10,24 @@ router.get("/user", UserController.index);
 router.get("/user/:id", UserController.show);
 router.post("/user", UserController.store);
 router.put("/user/:id", UserController.update);
+router.delete("/user/:id", UserController.delete);
 
 router.get("/list/user/:id", ListController.index);
 router.get("/list/:id", ListController.show);
 router.post("/list", ListController.store);
+router.put("/list/:id", ListController.update);
+router.delete("/list/:id", ListController.delete);
 
-router.get("/measurement", MeasurementController.index);
-router.get("/measurement/:id", MeasurementController.show);
-router.post("/measurement", MeasurementController.store);
+router.get("/measure", MeasureController.index);
+router.get("/measure/:id", MeasureController.show);
+router.post("/measure", MeasureController.store);
+router.put("/measure/:id", MeasureController.update);
+router.delete("/measure/:id", MeasureController.delete);
 
 router.get("/product/list/:id", ProductController.index);
 router.get("/product/:id", ProductController.show);
 router.post("/product", ProductController.store);
+router.put("/product/:id", ProductController.update);
+router.delete("/product/:id", ProductController.delete);
 
 module.exports = router;
