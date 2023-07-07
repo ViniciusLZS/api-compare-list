662be7dfb68ac0ed9ef9dc0936fbbbbdbb457012
Vinícius <vinicius.luiz.9256@gmail.com>
2023-07-06 22:15:48 -0300

    feat: added new origin

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,5 +1,11 @@
 module.exports = (request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  const allowedOrigins = ['http://localhost:3000', 'http://192.168.18.7:3000'];
+  const origin = request.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    response.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
   response.setHeader('Access-Control-Allow-Methods', '*');
   response.setHeader('Access-Control-Allow-Headers', '*');
   response.setHeader('Access-Control-Max-Age', '10');
