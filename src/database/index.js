50c57715a391b0c8dbc6d63d2cd6d57829a01392
Vinicius <vinicius.luiz.9256@gmail.com>
2023-08-21 15:54:43 -0300

    fix: database

--- src/database/index.js
+++ src/database/index.js
@@ -1,16 +1,26 @@
 const { Client } = require('pg');
-const DATABASE_URL = process.env.DATABASE_URL;
-const client = new Client({
-  host: DATABASE_URL,
-  port: 5432,
-  user: 'root',
-  password: 'root',
-  database: 'compare_list',
-});
+const url = require('url');
+
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(':');
+
+const config = {
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
+  ssl: {
+    rejectUnauthorized: false, // Certificado SSL (opcional, dependendo das configurações do seu banco de dados)
+  },
+};
+
+const client = new Client(config);
 
 client.connect();
 
 exports.query = async (query, values) => {
-  const {rows} = await client.query(query, values);
+  const { rows } = await client.query(query, values);
   return rows;
-}
+};
+
